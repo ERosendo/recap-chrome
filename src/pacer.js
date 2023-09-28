ERosendo/recap-chrome

commit e2a07cf985e6c052f649f8325f083d473bacacb4

refactor(recap extension): Refactor the logic to get the cookie value
- Use a different approach to get the value of the isFilingAccount cookie

src/pacer.js
@@ -413,14 +413,11 @@ let PACER = {
   },
 
   // Given document.cookie, returns true if the user has filing rights.
-  hasFilingCooking: function (cookieString){
-    let cookies = {};
-    cookieString.replace(/\s*([^=;]+)=([^;]*)/g, function (match, name, value) {
-      cookies[name.trim()] = value.trim();
-    });
-    let filingCookie = cookies['filing']
-    console.log(filingCookie)
-    return !!(filingCookie && filingCookie.match(/true/));
+  hasFilingCookie: function (cookieString){
+    let filingCookie = cookieString.split('; ')
+        .find((row) => row.startsWith('isFilingAccount'))
+        ?.split('=')[1];
+    return !!filingCookie.match(/true/);
   },
 
   // Returns true if the given court identifier is for an appellate court.

commit e09298f5a2f5bc6275e0eacb6791f82f7eef10d6

pacer.js: isFilingAccount may not exist
If the `isFilingAccount` cookie does not exist, this raised
`TypeError: filingCookie is undefined`.

Test filingCookieVal before`.match()` is called on `undefined`.

Note that this cookie is our own invention, so if we're expecting it
to be set and it is not, it's because we failed to set it. Perhaps it
should be named differently to make provenance clear (also why is it a
cookie?).

xxx: This was broken by e2a07cf9 which refactored this code (why?).

Fixes this (new) test:

Chrome Headless 110.0.5478.0 (Mac OS 10.15.7) The PACER module hasFilingCookie returns false for a valid logged in cookie without isFilingAccount FAILED
        TypeError: Cannot read properties of undefined (reading 'match')
            at Object.hasFilingCookie (src/pacer.js:605:27 <- src/pacer.js:265:303)
            at UserContext.<anonymous> (spec/PacerSpec.js:587:20)
            at <Jasmine>

src/pacer.js
@@ -606,7 +606,7 @@ let PACER = {
     let filingCookieStr = cookieString.split('; ')
       .find((row) => row.startsWith('isFilingAccount'));
     let filingCookieVal = filingCookieStr && filingCookieStr.split('=')[1];
-    return !!filingCookieVal.match(/true/);
+    return !!(filingCookieVal && filingCookieVal.match(/true/));
   },
 
   // Returns true if the given court identifier is for an appellate court.